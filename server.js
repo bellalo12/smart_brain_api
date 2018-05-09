fe20ec22d9d9b4c31b17041c842821d358a5f13e
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-05-09 13:06:44 +0700

    fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    connectinString : process.env.DATABASE_URL 
+    connectinString : process.env.DATABASE_URL,
     ssl: true
   }
 });
